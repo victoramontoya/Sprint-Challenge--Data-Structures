d2a51d31d5386623c752d0eaf5b9dcf034e70b07
Victor Montoya <victoramontoya@gmail.com>
2018-02-03 12:16:14 -0800

    Victor Montoya - Sprint Challenge - Data Structures - Incomplete

--- src/hash-table-helpers.js
+++ src/hash-table-helpers.js
@@ -3,6 +3,39 @@ class LimitedArray {
   constructor(limit) {
     // You should not be directly accessing this array from your hash table methods
     // Use the getter and setter methods included in this class to manipulate data in this class
+    this.storage = [];
+    this.limit = limit;
+  }
+
+  checkLimit(index) {
+    if (typeof index !== 'number') throw new Error('The supplied index needs to be a number');
+    if (this.limit <= index) {
+      throw new Error('The supplied index lies out of the array\'s bounds');
+    }
+  }
+
+  each(cb) {
+    for (let i = 0; i < this.storage.length; i++) {
+      cb(this.storage[i], i);
+    }
+  }
+  // Use this getter function to fetch elements from this class
+  get(index) {
+    this.checkLimit(index);
+    return this.storage[index];
+  }
+
+  get length() {
+    return this.storage.length;
+  }
+  // Use this setter function to add elements to this class
+  set(index, value) {
+    this.checkLimit(index);
+    this.storage[index] = value;
+  }
+}
+class LinkedList {
+  constructor() {
     this.head = null;
     this.tail = null;
     // Do not modify anything inside of the constructor
